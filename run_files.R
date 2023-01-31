f72c25aaf49d3d020fa1b818291a7532abb8f08c
Timo Dimitriadis <timo.dimi@googlemail.com>
2023-01-31 15:54:45 +0100

    Updated replication respoitory for the arXiv version v2 with the new title "Dynamic CoVaR Modeling".

--- run_files.R
+++ run_files.R
@@ -2,26 +2,16 @@
 ###  and the Applications
 
 
-### Simulations
-
-# Section 3.1: Predictive CoQR simulations
-source("simulations/sim_CoQR.R")
-source("simulations/sim_CoQR_eval.R")
-
-# Section 3.2: Dynamic CoCAViaR simulations
+### Section 3: Dynamic CoCAViaR Simulations
 source("simulations/sim_CoCAViaR.R")
 source("simulations/sim_CoCAViaR_eval.R")
 
 
-### Applications
+### Section 4: Forecasting application with dynamic CoCAViaR models
 
 # Important note: The following files only run with the publicly unavailable
 # data files mentioned in the README file.
 
-# Section 4.1: Predictive CoQR application
-source("application/CS_application.R")
-
-# Section 4.2: Forecasting application with dynamic CoCAViaR models
 source("application/FC_application_ModelParameters.R")
 source("application/FC_application_FCEvaluation.R")
 source("application/FC_application_ModelParameters.R")
